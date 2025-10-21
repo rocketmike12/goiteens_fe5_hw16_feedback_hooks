0d119c09b6a3f1e1adde6b2398440561f945cd92
rocketmike12 <mg.korop@gmail.com>
2025-10-21 19:42:58 +0300

    base cloned

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/goiteens_fe5_hw08/"
+	base: "/goiteens_fe5_hw16_feedback_hooks/"
 });
